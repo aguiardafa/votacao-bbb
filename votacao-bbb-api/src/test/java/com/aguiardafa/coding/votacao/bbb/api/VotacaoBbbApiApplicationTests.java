24aa085a1900b78b954c6e20c35981c5bc3cc3f0
aguiardafa <diego.fernandes.aguiar@gmail.com>
2022-03-18 11:52:18 -0300

    feat(init): cria projeto spring boot

---
+++ votacao-bbb-api/src/test/java/com/aguiardafa/coding/votacao/bbb/api/VotacaoBbbApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aguiardafa.coding.votacao.bbb.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotacaoBbbApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
